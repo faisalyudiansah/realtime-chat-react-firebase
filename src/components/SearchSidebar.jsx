b97e95bcbf4043f98c7160485455efe1b63152e2
faisalyudiansah <mfaisal.yudiansah@gmail.com>
2024-01-02 04:46:16 +0700

    feat: search user

--- src/components/SearchSidebar.jsx
+++ src/components/SearchSidebar.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import {
+  Input,
+  Avatar,
+} from "@material-tailwind/react";
+import { FaSearch } from "react-icons/fa";
 
 const SearchSidebar = () => {
   return (
-    <div>SearchSidebar</div>
-  )
+    <div className="w-full bg-green-800">
+      <div>
+        <Input
+          type='text'
+          className='rounded-none border-none focus:border-none text-green-100 '
+          placeholder='find a user...'
+          icon={<FaSearch className='text-green-100' />}
+        />
+      </div>
+      <div className='flex items-center gap-4 p-2 hover:bg-green-900 cursor-pointer'>
+        <Avatar
+          className="border border-green-100 p-0.5"
+          src="https://docs.material-tailwind.com/img/face-2.jpg"
+          alt="avatar" />
+        <div className='text-green-100'>
+          <span>Cristiano Ronaldo</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default SearchSidebar
+export default SearchSidebar;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavbarSideBar from './NavbarSidebar'
+import SearchSidebar from './SearchSidebar'
 
 function Sidebar() {
   return (
     <div className='w-1/3 bg-light-green-900 rounded-l-xl'>
       <NavbarSideBar />
+      <SearchSidebar />
     </div>
   )
 }
